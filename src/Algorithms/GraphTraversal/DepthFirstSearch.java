5999ef9ce347b0b8125a7d3a557ba3d1a7a4cd38
Virgil Nelson <mrvnelson@hotmail.com>
2019-04-30 20:10:52 +0100

    Passed small test maze

--- src/Algorithms/GraphTraversal/DepthFirstSearch.java
+++ src/Algorithms/GraphTraversal/DepthFirstSearch.java
@@ -5,10 +5,52 @@
  */
 package Algorithms.GraphTraversal;
 
+import Algorithms.GraphTraversal.GraphNode.State;
+import java.util.LinkedList;
+
 /**
  *
  * @author Personal
  */
 public class DepthFirstSearch {
     
+    public static LinkedList<GraphNode> getValidRoute(GraphMatrix maze)
+    {
+        LinkedList<GraphNode> validRoute = new LinkedList<>();
+        GraphNode startNode = maze.getStartNode();
+        startNode.setState(State.visited);
+        validRoute.add(startNode);
+        return traverse(validRoute, maze);
+    }
+    
+    public static LinkedList<GraphNode> traverse(LinkedList<GraphNode> validRoute, GraphMatrix maze)
+    {
+        int adjacentVisitedNodesNo = 0;
+        for(int[] adjNodesXY : validRoute.getLast().getAdjacentNodesPositions())
+        {
+            if(validRoute.getLast().getValue().equals("E"))
+                return validRoute;
+            
+            GraphNode nextNode = maze.getNode(adjNodesXY[0], adjNodesXY[1]);
+            if(nextNode.getState() == State.unvisited)
+            {
+                nextNode.setState(State.visited);
+                adjacentVisitedNodesNo++;
+                validRoute.add(nextNode);
+                
+                if(nextNode.getValue().equals("E"))
+                    return validRoute;
+                
+                validRoute = traverse(validRoute,maze);
+            }
+        }
+        if(adjacentVisitedNodesNo == 0)
+        {
+            if(validRoute.getLast().getValue().equals("S"))
+                throw new IllegalArgumentException("Maze has no valid route within it");
+            validRoute.removeLast();
+        }
+        
+        return validRoute;
+    }
 }
