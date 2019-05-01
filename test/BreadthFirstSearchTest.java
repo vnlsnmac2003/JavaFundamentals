3b695c3cb23bb83dffff6035ee3886284f8f0a3b
Virgil Nelson <mrvnelson@hotmail.com>
2019-05-01 16:10:42 +0100

    depth first search working

--- test/BreadthFirstSearchTest.java
+++ test/BreadthFirstSearchTest.java
@@ -6,6 +6,7 @@
 
 import Algorithms.GraphTraversal.BreadthFirstSearch;
 import Algorithms.GraphTraversal.BreadthFirstSearchNormal;
+import Algorithms.GraphTraversal.DepthFirstSearch;
 import Algorithms.GraphTraversal.GraphMatrix;
 import Algorithms.GraphTraversal.GraphNode;
 import Algorithms.GraphTraversal.GraphNode.State;
@@ -95,5 +96,44 @@ public class BreadthFirstSearchTest {
          quickestRoute.add(endNode);
          
          Assert.assertEquals(quickestRoute, BreadthFirstSearchNormal.getQuickestRoute(maze));
+     }
+     
+     @Test
+     public void validRouteDFS() {
+         //W,E,S,N
+         ArrayList<GraphNode> validRoute = new ArrayList<>();
+         
+         GraphNode startNode = new GraphNode(1,1,"S");
+         startNode.setAdjacentNodesPositions(new int[][]{{1,2},{2,1}});
+         startNode.setState(State.visited);
+         
+         GraphNode node2 = new GraphNode(1,2,"0");
+         node2.setAdjacentNodesPositions(new int[][]{{1,1},{1,3}});
+         node2.setState(State.visited);
+         
+         GraphNode node3 = new GraphNode(1,3,"0");
+         node3.setAdjacentNodesPositions(new int[][]{{1,2},{2,3}});
+         node3.setState(State.visited);
+         
+         GraphNode node4 = new GraphNode(2,3,"0");
+         node4.setAdjacentNodesPositions(new int[][]{{3,3},{1,3}});
+         node4.setState(State.visited);
+         
+         GraphNode node5 = new GraphNode(3,3,"0");
+         node5.setAdjacentNodesPositions(new int[][]{{4,3},{2,3}});
+         node5.setState(State.visited);
+         
+         GraphNode endNode = new GraphNode(4,3,"E");
+         endNode.setAdjacentNodesPositions(new int[][]{{3,3}});
+         endNode.setState(State.visited);
+         
+         validRoute.add(startNode);
+         validRoute.add(node2);
+         validRoute.add(node3);
+         validRoute.add(node4);
+         validRoute.add(node5);
+         validRoute.add(endNode);
+         
+         Assert.assertEquals(validRoute, DepthFirstSearch.getValidRoute(maze));
      }
 }
